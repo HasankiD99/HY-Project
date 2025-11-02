046a17885c574601878abe02270e50c94ec322ae
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2025-11-03 00:14:18 +0530

    banner img

--- components/HereSection.jsx
+++ components/HereSection.jsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <section className="flex flex-col items-start justify-center px-10 py-20 bg-[#F9F5FF]">
-      <h2 className="text-4xl font-bold leading-snug max-w-3xl">
+      <h2 className="text-4xl font-bold leading-snug max-w-3xl text-black">
         Your Trusted Partner for <br />
         Intelligent, <span className="text-purple-700">Future-Ready</span>{" "}
         Solutions
@@ -17,6 +19,17 @@ export default function HeroSection() {
       <button className="mt-6 bg-purple-600 text-white px-6 py-3 rounded-full hover:bg-purple-700 transition">
         Contact Our Sales →
       </button>
+
+      <div>
+        <Image
+          src="/assests/HY.svg"  
+          alt="Hy Logo"
+          width={40}
+          height={40}
+          priority
+        />
+
+      </div>
     </section>
   );
 }
